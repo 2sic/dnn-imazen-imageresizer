b43e3db12aef82502e672eb9f8c7ed8280751605
ijungleboy <daniel.mettler@2sic.com>
2020-04-27 21:27:43 +0200

    experimenting with WebP support, not yet published

--- Imazen ImageResizer for DNN by 2sic/Imazen ImageResizer for DNN by 2sic/Properties/AssemblyInfo.cs
+++ Imazen ImageResizer for DNN by 2sic/Imazen ImageResizer for DNN by 2sic/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Imazen ImageResizer for DNN by 2sic")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
